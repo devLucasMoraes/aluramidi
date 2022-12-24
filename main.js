d98c4e9b5ed60ae50a3abcff295291f5678d0463
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-23 22:51:48 -0300

    implementado fundionalidade de tocar som com o click do mouse

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (id) {
+    document.querySelector(id).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeteclas = document.querySelectorAll('.tecla')
 
+listaDeteclas.forEach(tecla => {
+    //template string
+    const id = `#som_${tecla.classList[1]}`
+    // função anônima ultilizada para poder passar um parâmetro para função tocaSom(), para esse atributo onclick, sem executá-la imediatamente.
+    tecla.onclick = () => tocaSom(id);
+})
